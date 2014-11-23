f6e50230beffd3a12e0594969d31db55b1320dcc
Azade Sanjari <asanjari@indiana.edu>
2014-11-23 01:27:17 -0500

    Notes and Codes for Week 3

--- Week3.R
+++ Week3.R
@@ -45,3 +45,37 @@ kmeansObj$cluster
 par(mar = rep(0.2, 4))
 plot(x,y, col = kmeansObj$cluster, pch = 19, cex = 2)
 points(kmeansObj$centers, col = 1:3, pch = 3, cex = 3, lwd = 3)
+
+-------------------
+Dimension Reduction
+-------------------
+        
+-----------------------
+Plotting and Color in R
+-----------------------
+#colorRamp
+pal <- colorRamp(c("red", "blue"))
+#[,1] [,2] [,3] corresponds to [Red] [Blue] [Green]
+pal(0)
+pal(1)
+pal(0.5)
+#colorRamp
+pal(seq(0, 1, len = 10))
+#colorRampPalette
+pal <- colorRampPalette(c("red", "yellow"))
+pal(2)
+pal(10)
+#RColorBrewer and colorRampPalette
+library(RColorBrewer)
+cols <- brewer.pal(3, "BuGn")
+cols
+pal <- colorRampPalette(cols)
+image(volcano, col = pal(20))
+#The smoothScatter function
+x <- rnorm(10000)
+y = rnorm(10000)
+smoothScatter(x, y)
+#Scatterplot with no transparency
+plot(x, y, pch = 19)
+#Scatterplot with transparency
+plot(x, y, col = rgb(0,0,0,0, 0.2), pch = 19)
